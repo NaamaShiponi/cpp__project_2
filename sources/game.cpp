61349cdeb80dfd40eb8bc459ac3289c771dedacd
Naama Shiponi <naamas1234567@gmail.com>
2023-03-26 23:36:16 +0300

    add fun to class game

---
+++ sources/game.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "game.hpp"
+#include "player.hpp"
+
+using namespace std;
+
+using namespace ariel;
+
+
+Game::~Game(){
+
+}
+void Game::playTurn(){
+    
+}
+void Game::printLastTurn(){
+
+}
+void Game::playAll(){
+
+}
+void Game::printWiner(){
+
+} 
+void Game::printLog(){
+
+} 
+void Game::printStats(){
+
+}
+
+
--- sources/game.hpp
+++ sources/game.hpp
@@ -1,25 +1,34 @@
+#ifndef GAME_H
+#define GAME_H
+#include "player.hpp"
 #include <iostream>
+
 
 using namespace std;
 
 namespace ariel {
     class Game {
-        Player* player1;
-        Player* player2;
+        Player player1;
+        Player player2;
 
 
     public:
-        Game() { this->player1 = new Player(); this->player2 = new Player(); }    // inline constructor
         ~Game();
-        Game(Player player1,Player player2);  // constructor
-
+        Game(Player player1,Player player2){
+            this->player1=player1;
+            this->player2=player2;
+        }
         void playTurn();
         void printLastTurn();
         void playAll();
         void printWiner(); 
         void printLog(); 
         void printStats();
-        
+
+
+
+
 
     };
 };
+#endif
